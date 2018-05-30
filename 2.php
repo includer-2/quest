bdfcb5e60f752eee6e7655cef737b24bb4bc3405
Artem <includer-2@yandex.ru>
2018-05-30 20:33:08 +0000

    Add 2.php

---
+++ 2.php
@@ -0,0 +1,42 @@
+<?php
+  include ('connect.php');
+  $path = __FILE__;
+  $file = basename($path, ".php");
+  $query = "SELECT QUESTION FROM `question` WHERE ID=$file";
+  $result = mysqli_query($db, $query); 
+  while($row=mysqli_fetch_array($result))
+  {
+    $answer = strtoupper($row['QUESTION']);
+  }
+?>
+<html>
+    <head>
+        <link href="css/main.css" rel="stylesheet">
+        <title>Part II</title>
+    </head>
+  <body>
+    <div class="container">
+      <div class="row content">
+        <div class="col-md-6 ff text-center" align=center>
+          <div class='hint'><?php echo $answer; ?></div>
+          <img src="https://qph.fs.quoracdn.net/main-qimg-c51824c3579eb96c436345ce433058e3" width="250"></img>
+          <p id="nome">white</p>
+          <form name="key" align=center action="" method=post>
+            <input type="text" name="key" class="text-center" placeholder="key"><br><br>
+            <button type="submit">Go!</button>
+              <?php
+              	if(strtoupper($_POST['key']) == $answer):
+              	  header( "Location: ./fun/good.html" );
+              	elseif ($_POST['key']!=$answer): {
+                  echo "<br / >".strtoupper($_POST['key']).": не верно";
+              	}
+              	endif;
+              ?>
+          </form>
+        </div>
+      </div>
+    </div>
+    <a href="index.html" class="link home-link">HOME</a>
+    <!-- key="How are you?" -->
+  </body>
+</html>
